ac2230b34d5471f22f34227a24b09c2abcbbb2d4
lucky <takuto.yoshikai@gmail.com>
2021-03-05 00:17:57 +0900

    package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function start() {
   setInterval(() => {
     console.log("work!");
-    setInterval(() => {
+    setTimeout(() => {
       console.log("take a break!");
     }, 25 * 60 * 1000);
   }, 30 * 60 * 1000);
